1130125544dc0b1df5012c14e3e958d6d6876d54
ryan <ryanjwalker2001@gmail.com>
2021-11-04 14:21:31 +0000

    improved prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -2,16 +2,26 @@ autoload -Uz add-zsh-hook zcs_info
 setopt prompt_subst
 
 zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr 'M' # Symbol for unstaged changes
+zstyle ':vcs_info:*' stagedstr 'S' # Symbol for staged changes
 zstyle ':vcs_info:git:*' formats "%{$fg[blue]%}(%{$fg[red]%}%m%u%c%{$fg[yellow]%} %{$fg[magenta]%} %b%{$fg[blue]%})"
-#zstyle ':vcs_info:git:*' formats '(%b %u%c)'
 zstyle ':vcs_info:git:*' actionformats '(%b|%a%u%c)'
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
 
-add-zsh-hook precmd() { 
-  vcs_info
-  print -nP '%F{blue}[%F{white}%n%F{red}@%F{white}%m%F{blue}] ' # [user@hostname]
-  print -nP '%F{green}➜ %F{cyan}%1~ ' # ➜ directory
-  print -P '%F{red}${vcs_info_msg_0_}' # version control
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+     git status --porcelain | grep -m 1 '^??' &>/dev/null
+  then
+    hook_com[misc]='!' # Symbol for untracked files
+  fi
 }
-PROMPT='%F{cyan}>%f '
 
+add-zsh-hook precmd vcs_info
+
+NEWLINE=$'\n'
+
+PROMPT='%F{blue}[%F{white}%n%F{red}@%F{white}%m%F{blue}] ' # [user@host]
+PROMPT+='%F{green}➜ %F{cyan}%1~ ' # ➜ directory
+PROMPT+='%F{red}${vcs_info_msg_0_} ' # (git info)
+PROMPT+='${NEWLINE}%F{cyan}>%f ' # > 
 
