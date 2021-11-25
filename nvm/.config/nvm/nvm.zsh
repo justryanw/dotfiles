ba4e7ccc60b1c209fe40307b7bf0579f409ece62
ryan <ryanjwalker2001@gmail.com>
2021-11-25 11:32:18 +0000

    nvmrc

--- nvm/.config/nvm/nvm.zsh
+++ nvm/.config/nvm/nvm.zsh
@@ -2,10 +2,26 @@ export NVM_DIR="$HOME/.nvm"
 
 lazynvm() {
   unset -f nvm node npm npx
-  export NVM_DIR=~/.nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
   if [ -f "$NVM_DIR/bash_completion" ]; then
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+  fi
+  load-nvmrc
+}
+
+load-nvmrc() {
+  local node_version="$(nvm version)"
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_contents=$(cat "${nvmrc_path}")
+    local nvmrc_node_version=$(nvm version $nvmrc_contents)
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$node_version" ]; then
+      nvm use
+    fi
   fi
 }
 
